97aa7415fa40596d3ba5fab06f6891643762a02b
JosephSwearingen <jhsweargn@gmail.com>
2023-01-24 18:07:21 -0800

    Fixed button binding 2.0

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -70,14 +70,14 @@ public class RobotContainer {
 
   private void configureBooleanSuppliers() {
     booleanSupplyLT = () -> {
-      if (driverController.getRawAxis(Constants.XBOX_LTriggerPort) > 0 && driverController.getRawAxis(Constants.XBOX_RTriggerPort) < 0) {
+      if (driverController.getRawAxis(Constants.XBOX_LTriggerPort) > 0.1 && driverController.getRawAxis(Constants.XBOX_RTriggerPort) < 0.1) {
         return true;
       } else {
         return false;
       }
     };
     booleanSupplyRT = () -> {
-      if (driverController.getRawAxis(Constants.XBOX_RTriggerPort) > 0 && driverController.getRawAxis(Constants.XBOX_LTriggerPort) < 0) {
+      if (driverController.getRawAxis(Constants.XBOX_RTriggerPort) > 0.1 && driverController.getRawAxis(Constants.XBOX_LTriggerPort) < 0.1) {
         return true;
       } else {
         return false;
