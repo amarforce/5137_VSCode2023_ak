50cbf99d91df9f82906836750226b2c4bfbf1cc8
Kianna D <kiannad1000@gmail.com>
2023-02-16 19:22:50 -0800

    started creating paths with PathPlanner

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Joystick;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -30,6 +38,9 @@ public class Robot extends TimedRobot {
   SendableChooser<String> driverControlChooser = new SendableChooser<>();
   SendableChooser<String> assistControlChooser = new SendableChooser<>();
 
+  String trajectoryJSON = "paths/YourPath.wpilib.json"; //change to path name later
+  Trajectory trajectory = new Trajectory();
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -52,6 +63,15 @@ public class Robot extends TimedRobot {
     
     SmartDashboard.putData("Driver Controller Type", driverControlChooser);
     SmartDashboard.putData("Assist Controller Type", assistControlChooser);
+
+    //loads in paths from PathWeaver
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+    } 
+    catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+    }
   }
 
   /**
