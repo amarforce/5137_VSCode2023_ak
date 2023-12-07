53701835cc87b02718c8d6dc03b16d6b374697f4
amarforce <amarvk@gmail.com>
2023-12-07 04:44:46 +0000

    removed dependencies

--- src/main/java/frc/robot/simulation/JMoneyEncoder.java
+++ src/main/java/frc/robot/simulation/JMoneyEncoder.java
@@ -1,9 +1,38 @@
+/*
+The selected code is a Java class named JMoneyEncoder that simulates an encoder measuring the displacement of a motor. This class is part of a package named frc.robot.simulation and it uses the CANSparkMax class from the com.revrobotics package.
+
+The JMoneyEncoder class has three instance variables:
+
+m_motor of type CANSparkMax, which represents the motor that the encoder is linked to.
+distance, a Double that keeps track of the total displacement of the motor.
+rotationPerUpdate, a Double that represents the amount of rotation per update.
+The class has a constructor that takes a CANSparkMax motor and a Double timePerRotation as parameters. The constructor initializes m_motor with the provided motor, distance with 0.0, and rotationPerUpdate with the value of 0.02/timePerRotation.
+
+There are three methods in this class:
+
+update(): This method updates the distance by adding the product of the current motor speed (m_motor.get()) and rotationPerUpdate.
+getPosition(): This method returns the current distance, which represents the total displacement of the motor.
+reset(): This method resets the distance to 0.0.
+This class is part of a Gradle project, which is a build automation tool used primarily for Java projects. Gradle helps manage dependencies, run tests, create documentation, and perform many other tasks.
+*/
+
+
 package frc.robot.simulation;
 
 import com.revrobotics.CANSparkMax;
 
+
+
+/**
+ * The JMoneyEncoder class simulates an encoder measuring the displacement of a motor.
+ * This class is intended for temporary use and was created for fun.
+ * -- JMoney
+ */
+
+
+
 public class JMoneyEncoder {
-    public CANSparkMax m_motor;
+    public CANSparkMax m_motor; 
     private Double distance;
     private Double rotationPerUpdate;
 
